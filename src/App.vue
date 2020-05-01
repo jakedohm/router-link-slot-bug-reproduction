aeea1c54eb341bc37b0f026aa4cad3a4feeb8130
Jake Dohm <jake.dohm@me.com>
2020-05-01 09:02:02 -0400

    Showing issue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link
+        v-slot="{ isActive }"
+        :class="[isActive ? 'bg-red' : 'bg-blue']"
+        active-class="underline"
+        to="/"
+        >Home</router-link
+      >
+      |
+      <router-link
+        v-slot="{ isActive }"
+        :class="[isActive ? 'bg-red' : 'bg-blue']"
+        active-class="underline"
+        to="/about"
+        >About</router-link
+      >
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#nav {
+  padding: 20px;
+}
+a {
+  color: white;
+  padding: 10px 20px;
+  border-radius: 5px;
+  text-decoration: none;
+}
+
+.bg-red {
+  background: red;
+}
+
+.bg-blue {
+  background: blue;
+}
+
+.underline {
+  text-decoration: underline;
 }
 </style>
